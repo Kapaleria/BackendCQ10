7ea6f316a72ccabe312d6ad5590954292d3f0daf
irene-nakabugo <irenenakabugo0@gmail.com>
2024-03-18 09:55:44 +0300

    dot env added

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 //create an express app
 const express =require('express')
 const app = express();
+require('dotenv/config') //SECURITY PURPOSES
 
 //ROUTES
 app.get('/',(req,res)=>{
     res.send("I am here")
 })
-
-
-
-
-app.listen(4000, ()=>{console.log('app running at 4000')})
+app.listen(process.env.PORT, ()=>{console.log('app running at 4000')})
