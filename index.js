f3f4af05f2d08fedb6328350a139ad3dea2f6752
irene-nakabugo <irenenakabugo0@gmail.com>
2024-03-25 11:32:43 +0300

    middleware

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 //create an express app
 const express =require('express')
+const mongoose =require('mongoose')
+const bodyParser=require('body-parser')
 const app = express();
-require('dotenv/config') //SECURITY PURPOSES
+require('dotenv/config') //security purposes
+
+const routes=require('./routes')
+app.use(bodyParser.json())
+app.use('/', routes)
+
+//CONNECT TO THE DATABASE
+mongoose.connect(process.env.DBLINK)
+.then(() => console.log('Connected successfully to the Database'))
+.catch((err) => { console.error(err); });
 
 //ROUTES
 app.get('/',(req,res)=>{
-    res.send("I am here")
-})
-app.listen(process.env.PORT, ()=>{console.log('app running at 4000')})
+    res.send("I am here")})
+
+
+app.listen(process.env.PORT, ()=>{console.log('app running')})
